56d80d6369922ec7c7485ec3bf3a3c90effac02e
Henry Lu <henry11241@gmail.com>
2024-05-27 16:37:27 +0800

    Create basic layout

---
+++ src/components/console.tsx
@@ -0,0 +1,7 @@
+export default function Console() {
+  return (
+    <div className="flex items-center justify-center h-2/5 text-white m-4 p-4 border rounded-2xl">
+      <h1>I am console</h1>
+    </div>
+  )
+}
---
+++ src/components/keyboard.tsx
@@ -0,0 +1,7 @@
+export default function Keyboard() {
+  return (
+    <div className="flex items-center justify-center h-3/5 text-white m-4 p-4 border rounded-2xl">
+      <h1>I am keyboard</h1>
+    </div>
+  )
+}
