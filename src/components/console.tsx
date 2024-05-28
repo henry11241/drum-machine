400aad1202566a44f8f30b8b80459f80d451383e
Henry Lu <henry11241@gmail.com>
2024-05-28 16:08:48 +0800

    Finish the layout style and add sound files

--- src/components/console.tsx
+++ src/components/console.tsx
@@ -1,7 +1,28 @@
 export default function Console() {
   return (
-    <div className="flex items-center justify-center h-2/5 text-white m-4 p-4 border rounded-2xl">
-      <h1>I am console</h1>
+    <div className="flex flex-col items-center justify-center gap-1 h-2/5 text-white m-4 mb-0 p-4 bg-slate-700 rounded-2xl">
+      <div className="switch"></div>
+      <h2>Power</h2>
+      <label
+        htmlFor="switch"
+        className="relative h-8 w-14 mb-1 cursor-pointer rounded-full bg-gray-300 transition [-webkit-tap-highlight-color:_transparent] has-[:checked]:bg-green-500 scale-75"
+      >
+        <input type="checkbox" id="switch" className="peer sr-only" />
+
+        <span className="absolute inset-y-0 start-0 m-1 size-6 rounded-full bg-white transition-all peer-checked:start-6"></span>
+      </label>
+      <div className="display flex items-center justify-center w-40 h-8 mb-1 text-black text-2xl font-bold bg-slate-400">
+        <span id="display-text">test</span>
+      </div>
+      <h2>Volume</h2>
+      <input
+        type="range"
+        id="volumeSlider"
+        min="0"
+        max="1"
+        step="0.01"
+        value="1"
+      />
     </div>
   )
 }
--- src/components/keyboard.tsx
+++ src/components/keyboard.tsx
@@ -1,7 +1,13 @@
 export default function Keyboard() {
+  const keys = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C']
+
   return (
-    <div className="flex items-center justify-center h-3/5 text-white m-4 p-4 border rounded-2xl">
-      <h1>I am keyboard</h1>
+    <div className="grid grid-rows-3 grid-cols-3 justify-center h-3/5 text-white m-4 p-4 rounded-2xl">
+      {keys.map((key) => (
+        <button className="m-1 relative rounded-xl bg-slate-900 shadow-md shadow-red-600 active:shadow-lg active:shadow-red-300 active:top-1">
+          {key}
+        </button>
+      ))}
     </div>
   )
 }
